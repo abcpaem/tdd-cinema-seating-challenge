69c96f3b5a2761791c0ab39f1520bf4383dc1e0b
abcpaem <abcpaem@gmail.com>
2022-04-28 18:29:16 +0100

    Refactors TicketingTests to apply DRY principle

--- src/main/java/clan/techreturners/Cinema.java
+++ src/main/java/clan/techreturners/Cinema.java
@@ -1,11 +1,6 @@
 package clan.techreturners;
 
 public class Cinema extends Venue {
-
-    public Cinema(String name) {
-        super(name, 0);
-    }
-
     public Cinema(String name, String[] rows, int seatsPerRow) {
         super(name, rows.length * seatsPerRow);
     }
--- src/test/java/clan/techreturners/TicketingTests.java
+++ src/test/java/clan/techreturners/TicketingTests.java
@@ -1,36 +1,28 @@
 package clan.techreturners;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class TicketingTests {
     private static final String VENUE_NAME = "Cinnamon";
+    private Venue cinema;
+
+    @BeforeEach
+    void Init() {
+        String[] rows = new String[3];
+        int seatsPerRow = 5;
+        cinema = new Cinema(VENUE_NAME, rows, seatsPerRow);
+    }
 
     @Test
     void checkCinemaName() {
-        // Arrange
-        Venue cinema = new Cinema(VENUE_NAME);
-
-        // Act
-        String venueName = cinema.getName();
-
-        // Assert
-        assertEquals(VENUE_NAME, venueName);
+        assertEquals(VENUE_NAME, cinema.getName());
     }
 
     @Test
     void checkCinemaCapacity() {
-        // Arrange
-        String[] rows = new String[3];
-        int seatsPerRow = 5;
-        int expectedCapacity = 15;
-        Venue cinema = new Cinema(VENUE_NAME, rows, seatsPerRow);
-
-        // Act
-        int capacity = cinema.getCapacity();
-
-        // Assert
-        assertEquals(expectedCapacity, capacity);
+        assertEquals(15, cinema.getCapacity());
     }
 }
